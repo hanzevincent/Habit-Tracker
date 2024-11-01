7b6c602c0e6b433798194f6b7ed7a4f69bd18c61
Thomas McKinstry <mckinst@wwu.edu>
2024-10-31 22:46:00 -0700

    Added barebones UI for index.tsx (Home) page. Currently non-functional.
    List of existing activities is represented by a hardcoded array. This will hopefully be replaced with items from the
    database once that's set up. It's implemented using a FlatList component, which should allow scrolling but is not
    at the moment. I will attempt to debug at a later date. All components are styled with a stylesheet at the bottom
    of the page, which should hopefully make refactoring easier later on. Buttons are also made using a custom component
    for the same reason.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,105 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { Link } from "expo-router";
+import { FlatList, ScrollView } from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+
+      <View style={styles.headerBar}>
+        <Text style={styles.headerButton}>Home</Text>
+
+        <Link href="help" style={styles.headerButton}>Help</Link>
+        <Link href="settings" style={styles.headerButton}>Settings</Link>
+      </View>
+
+      <View>
+        <Text style={styles.calendar}>
+          Calendar Placeholder
+        </Text>
+      </View>
+
+      <View>
+        <NavigationButton name="Create New Activity" link="newActivityForm"/>
+
+        <NavigationButton name="Log Activity" link="newActivityForm"/>
+      </View>
+
+      <View>
+
+        <Text style={styles.listHeader}>Activities</Text>
+
+        <FlatList
+          data = {ActivityList}
+          renderItem={({item}) => <NavigationButton name={item} link="newActivityForm"/>}
+        />
+      </View>
+      
     </View>
   );
 }
+
+const NavigationButton = props => {
+  return (
+  <TouchableHighlight>
+    <Link href={props.link} style={styles.button}>{props.name}</Link>
+  </TouchableHighlight>
+  );}
+
+const ActivityList = [
+  "Read Book",
+  "Exercise",
+  "Homework",
+  "Clean Room",
+  "Wash Dishes",
+  "Read Book",
+  "Exercise",
+  "Homework",
+  "Clean Room",
+  "Wash Dishes",
+  "Read Book",
+  "Exercise",
+  "Homework",
+  "Clean Room",
+  "Wash Dishes",
+];
+
+const styles = StyleSheet.create({
+  button: {
+    textAlign: 'center',
+    padding: 15,
+    margin: 10,
+    borderRadius: 5,
+    backgroundColor: "#a5c6fa",
+  },
+
+  headerBar: {
+    flexDirection: 'row',
+    margin: 5,
+  },
+
+  headerButton: {
+    backgroundColor: "#a5c6fa",
+    flex: 1,
+    textAlign: "center",
+    margin: 5,
+    padding: 15,
+    borderRadius: 5,
+  },
+
+  calendar: {
+    width: 'auto',
+    height: 250,
+    padding: 25,
+    textAlign: "center",
+    backgroundColor: "#a5c6fa",
+    borderRadius: 5,
+    margin: 10,
+  },
+
+  listHeader: {
+    width: 'auto',
+    textAlign: 'center',
+  },
+});
