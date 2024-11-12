14eda564569c831b023aa3ce92985941eb8976ba
Clare Gaskill <cgaskill.2021@bishopblanchet.org>
2024-11-12 13:18:59 -0800

    new activity form basic functionality

---
+++ app/NewActivityForm.tsx
@@ -0,0 +1,9 @@
+import React , {useState, Component} from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { View, Text, TextInput, Button, StyleSheet, SafeAreaView } from 'react-native';
+
+function NewActivityForm(){
+    <form>
+        
+    </form>
+}
